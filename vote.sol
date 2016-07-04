6398ddfec1748c6749c30e9f8f5425f7eb814d12
Dennis Ahlqvist <dennisahlqvist@hotmail.com>
2016-07-04 19:14:34 +0300

    Added voting

--- vote.sol
+++ vote.sol
@@ -1,8 +1,32 @@
 contract Vote {
   address owner;
+  enum VoteTypes { Yes, No }
+  mapping (address => VoteTypes) votes;
+  address[] voted;
   function Vote() {
     owner=msg.sender;
+  }
 
+  function voteYes() returns (bool success) {
+    if(!addressInArray(msg.sender)){voted.push(msg.sender);}
+    votes[msg.sender] = VoteTypes.Yes;
+    return true;
+  }
+
+  function voteNo() returns (bool success) {
+    if(!addressInArray(msg.sender)){voted.push(msg.sender);}
+    votes[msg.sender]=VoteTypes.No;
+    return true;
+  }
+
+
+  function addressInArray(address inAddress) private returns (bool inArray){
+    for(uint i=0; i<voted.length;i++){
+      if(voted[i]==inAddress){
+        return true;
+      }
+    }
+    return false;
   }
   function kill() {
     if (msg.sender==owner){
